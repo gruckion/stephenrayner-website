4046260fd5f0edd4f2c15bcf4d23a197ce5c0f54
Stephen Rayner <stephen.r.rayner@gmail.com>
2025-09-13 15:28:36 +0100

    feat: colours and content improvement

--- src/config/about.ts
+++ src/config/about.ts
@@ -1,56 +1,38 @@
 interface HobbiesProps {
   image: string;
   alt: string;
+  orientation: "portrait" | "landscape";
 }
 
 export const hobbies: HobbiesProps[] = [
-  // first column
   {
-    image:
-      "https://images.pexels.com/photos/15372903/pexels-photo-15372903/free-photo-of-computer-setup-with-big-monitor-screen.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    alt: "setup desktop",
+    image: "/roy.jpg",
+    alt: "Roy the dog",
+    orientation: "landscape",
   },
   {
-    image:
-      "https://images.pexels.com/photos/1049317/pexels-photo-1049317.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    alt: "friends smiles",
-  },
-  // second column
-  {
-    image:
-      "https://images.pexels.com/photos/3712095/pexels-photo-3712095.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    alt: "grey cat",
+    image: "/kiki.jpg",
+    alt: "Kiki the dog - Achilles",
+    orientation: "portrait",
   },
   {
-    image:
-      "https://images.pexels.com/photos/9293249/pexels-photo-9293249.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    alt: "home building",
+    image: "/troy.jpg",
+    alt: "Troy the dog",
+    orientation: "landscape",
   },
   {
-    image:
-      "https://images.pexels.com/photos/375467/pexels-photo-375467.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    alt: "pizza laptop",
-  },
-  // third column
-  {
-    image:
-      "https://images.pexels.com/photos/1230302/pexels-photo-1230302.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    alt: "hike and sunset",
+    image: "/juno.jpg",
+    alt: "Juno the dog",
+    orientation: "portrait",
   },
   {
-    image:
-      "https://images.pexels.com/photos/5500779/pexels-photo-5500779.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    alt: "chinese lantern",
-  },
-  // fourth column
-  {
-    image:
-      "https://images.pexels.com/photos/2090644/pexels-photo-2090644.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    alt: "the great wheel",
+    image: "/java.jpg",
+    alt: "Java the dog",
+    orientation: "landscape",
   },
   {
-    image:
-      "https://images.pexels.com/photos/7418632/pexels-photo-7418632.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    alt: "dalmatian",
+    image: "/my_girls.jpg",
+    alt: "My girls - Lucy, Java, Juno, and Human",
+    orientation: "landscape",
   },
 ];
