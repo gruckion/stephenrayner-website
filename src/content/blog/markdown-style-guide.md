a841f8d0cedc165dc88ac8db9edc4cfae17933d1
Stephen Rayner <stephen.r.rayner@gmail.com>
2025-09-13 19:45:53 +0100

    feat: toggle blog posts and site url on experiences

--- src/content/blog/markdown-style-guide.md
+++ src/content/blog/markdown-style-guide.md
@@ -4,6 +4,7 @@ description: "Here is a sample of some basic Markdown syntax that can be used wh
 pubDate: "Jul 01 2022"
 cover: "/images/blog/placeholder-1.jpg"
 category: "code"
+published: false
 ---
 
 Here is a sample of some basic Markdown syntax that can be used when writing Markdown content in Astro.
--- src/lib/fetchers.ts
+++ src/lib/fetchers.ts
@@ -1,7 +1,7 @@
 import { getCollection } from "astro:content";
 
 export async function getCategories() {
-  const posts = await getCollection("blog");
+  const posts = (await getCollection("blog")).filter((post) => post.data.published);
   const categories = [
     ...new Set(posts.map((post) => post.data.category).flat()),
   ];
@@ -10,25 +10,17 @@ export async function getCategories() {
 }
 
 export async function getPosts() {
-  const posts = (await getCollection("blog")).sort(
-    (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
-  );
+  const posts = (await getCollection("blog"))
+    .filter((post) => post.data.published)
+    .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
 
   return posts;
 }
 
 export async function getPostsByCategory(category: string) {
   const posts = (await getCollection("blog"))
-    .filter((post) => post.data.category.includes(category))
+    .filter((post) => post.data.published && post.data.category.includes(category))
     .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
 
   return posts;
-}
-
-export async function getGuides() {
-  const guides = (await getCollection("guides"))
-    .filter((guide) => guide.data.published)
-    .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
-
-  return guides;
 }
